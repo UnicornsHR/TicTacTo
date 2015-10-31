b3f86cb8735070469abae6018aaa132993a1e3d9
Perla Osk Hjartardottir <perla@i-7f3b5dd9.csd07cloud.internal>
2015-10-31 15:42:57 +0000

    changePlayer and playGame

--- src/test/java/MyUnitTest.java
+++ src/test/java/MyUnitTest.java
@@ -6,12 +6,12 @@ import static org.junit.Assert.*;
 public class MyUnitTest {
 
 	@Test
-	public void FirstPlayerMarkTest() {
+	public void firstPlayerMarkTest() {
 		TTTGame TTT = new TTTGame();
 		assertEquals('x', TTT.currPlayerMark);
 	}
 	@Test
-	public void BoardSizeTest() {
+	public void boardSizeTest() {
 		TTTGame TTT = new TTTGame();
 		assertEquals(3, TTT.board.length);
 		for(int r = 0; r < TTT.board.length; r++)
@@ -20,16 +20,41 @@ public class MyUnitTest {
 		}
 	}
 	@Test
-	public void ResetBoardTest() {
+	public void resetBoardTest() {
 		TTTGame TTT = new TTTGame();
 		for(int r = 0; r < 3; r++) {
 			for(int c = 0; c < 3; c++) {
 				assertEquals(' ', TTT.board[r][c]);
 			}
 		}
-	}
 
-/* @Test
+	}
+	@Test
+	public void changePlayerTest() {
+		TTTGame TTT = new TTTGame();
+		TTT.changePlayer();
+		assertEquals('o', TTT.currPlayerMark);
+		TTT.changePlayer();
+		assertEquals('x', TTT.currPlayerMark);
+	}
+	@Test
+	public void illegalPositionPlayGameTest() {
+		TTTGame TTT = new TTTGame(); 
+		assertEquals(false, TTT.playGame(-1, 0));
+		assertEquals(false, TTT.playGame(0, -1));
+		assertEquals(false, TTT.playGame(-1, -1));
+		assertEquals(false, TTT.playGame(3, 0));
+		assertEquals(false, TTT.playGame(0, 3));
+		assertEquals(false, TTT.playGame(3, 3));
+	}
+	@Test
+	public void cellAlreadyTakenPlayGameTest() {
+		TTTGame TTT = new TTTGame();
+		assertEquals(true, TTT.playGame(0, 0));
+		TTT.playGame(0, 0);
+		assertEquals(false, TTT.playGame(0, 0));
+	}
+/*@Test
     public void testHello() {
         Greeter greetertest= new Greeter();
 
