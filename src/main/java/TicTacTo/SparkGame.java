2a02ac062fb456eaaed415fa294781fcfcb4b4c9
Karlotta Gudlaugsdottir <karlotta14@i-7f3b5dd9.csd07cloud.internal>
2015-11-03 14:40:23 +0000

    leikur virkar aftur, build skrar i lagi

--- src/main/java/TicTacTo/SparkGame.java
+++ src/main/java/TicTacTo/SparkGame.java
@@ -1,4 +1,51 @@
 package tictacto;
+import java.util.Scanner;
+
+public class SparkGame {
+  public static void main(String[] args) { 
+
+	int playAgain = 1;
+	while (playAgain == 1) {
+		TTTGame TicTacToe = new TTTGame();
+		TicTacToe.printBoard();
+		Scanner in = new Scanner(System.in);
+
+		while (TicTacToe.isBoardFull() == false && TicTacToe.isThereAWinner() == false) {
+
+			int x = in.nextInt();
+			int y = in.nextInt();
+
+			while(((x < 0) ||( x > 2)) || ((y < 0) || (y > 2))) {
+				System.out.println("please enter numbers between 0 and 2");
+				x = in.nextInt();
+				y = in.nextInt();
+			}
+
+			while (TicTacToe.isSpotMarked(x, y) == true) {
+                                        System.out.println ("This spot is already marked, choose an unmarked spot");
+                                        x = in.nextInt();
+                                        y = in.nextInt();
+                        }
+
+			TicTacToe.playGame(x,y);
+			TicTacToe.printBoard();
+			if (TicTacToe.isThereAWinner() == true) {
+				System.out.println("We have a winner!");
+			}
+
+			else if (TicTacToe.isBoardFull() == true) {
+				System.out.println("No winner...");
+			}
+
+			TicTacToe.changePlayer();
+		}
+
+		System.out.println("Choose 1 if you want to play again");
+		playAgain = in.nextInt();
+	}
+  }
+}
+/*package tictacto;
 import static spark.Spark.*;
 import spark.*;
 import spark.servlet.SparkApplication;
@@ -23,5 +70,47 @@ public class SparkGame implements SparkApplication{
 			return response;
 			}
 		});
+		/*post(new Route("/playerMove") {
+		@Override
+		public Object handle(Request request, Responce responce) {
+
+			int playAgain = 1;
+			while (playAgain == 1) {
+				TTTGame TicTacToe = new TTTGame();
+				TicTacToe.printBoard();
+				Scanner in = new Scanner(System.in);
+
+			while (TicTacToe.isBoardFull() == false && TicTacToe.isThereAWinner() == false) {
+
+				int x = in.nextInt();
+				int y = in.nextInt();
+
+				while(((x < 0) ||( x > 2)) || ((y < 0) || (y > 2))) {
+					System.out.println("please enter numbers between 0 and 2");
+					x = in.nextInt();
+					y = in.nextInt();
+			}
+
+			while (TicTacToe.isSpotMarked(x, y) == true) {
+                                        System.out.println ("This spot is already marked, choose an unmarked spot");
+                                        x = in.nextInt();
+                                        y = in.nextInt();
+                        }
+
+			TicTacToe.playGame(x,y);
+			TicTacToe.printBoard();
+			if (TicTacToe.isThereAWinner() == true) {
+				System.out.println("We have a winner!");
+			}
+
+			else if (TicTacToe.isBoardFull() == true) {
+				System.out.println("No winner...");
+			}
+
+			TicTacToe.changePlayer();
+		}
+
+		System.out.println("Choose 1 if you want to play again");
+		playAgain = in.nextInt();
 	}
-}
+}*/
